e1029734eaa897dca9208388b53571ac722272ec
yuk6ra <yuki.imamura.all@gmail.com>
2022-10-11 13:44:51 +0900

    eth

---
+++ hardhat.config.js
@@ -0,0 +1,31 @@
+// hardhat.config.js
+require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-etherscan");
+require("dotenv").config();
+
+const { API_URL, PRIVATE_KEY, ETHERSCAN_API, POLYGON_URL } = process.env;
+
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+module.exports = {
+  solidity: "0.8.9",
+  networks: {
+    goerli: {
+      url: API_URL,
+      accounts: [PRIVATE_KEY],
+    },
+    mumbai: {
+      url: POLYGON_URL,
+      accounts: [PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API,
+  },
+};
---
+++ scripts/deploy.js
@@ -0,0 +1,42 @@
+// run.js
+async function main() {
+  // あなたのコレクションの Base Token URI（JSON の CID）に差し替えてください
+  const baseTokenURI = "ipfs://QmXUP1fVMjbD9rcXnBL2fzFZ9zAJgCMey3tzdBw4z6X8VQ/";
+
+  // オーナー/デプロイヤーのウォレットアドレスを取得する
+  const [owner] = await hre.ethers.getSigners();
+
+  // デプロイしたいコントラクトを取得
+  const contractFactory = await hre.ethers.getContractFactory("NFTCollectible");
+
+  // 正しいコンストラクタ引数（baseTokenURI）でコントラクトをデプロイします。
+  const contract = await contractFactory.deploy(baseTokenURI);
+
+  // このトランザクションがマイナーに承認（mine）されるのを待つ
+  await contract.deployed();
+
+  // コントラクトアドレスをターミナルに出力
+  console.log("Contract deployed to:", contract.address);
+
+  // NFTを 10 点、コントラクト所有者のためにキープする
+  let txn = await contract.reserveNFTs();
+  await txn.wait();
+  console.log("10 NFTs have been reserved");
+
+  // 0.03 ETH を送信して3つ NFT を mint する
+  txn = await contract.mintNFTs(3, {
+    value: hre.ethers.utils.parseEther("0.03"),
+  });
+  await txn.wait();
+
+  // コントラクト所有者の保有するtokenIdsを取得
+  let tokens = await contract.tokensOfOwner(owner.address);
+  console.log("Owner has tokens: ", tokens);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
